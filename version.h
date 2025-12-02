81b0ba0bbf57d89a3065738dd28bc4ed7cf2fce0
jason <jkbenaim@gmail.com>
2025-12-02 21:40:19 +0000

    version 0.2
    FossilOrigin-Name: 3e02629dcadcc991e0e712806db436fbd83d212c0931756df80dcb28a19b06a6

--- version.h
+++ version.h
@@ -2,9 +2,9 @@
 #define PROG_NAME "uninst"
 #define PROG_URL "https://jrra.zone/uninst"
 #define PROG_EMAIL "jkbenaim@gmail.com"
-#define PROG_VERSION_STRING "0.1"
+#define PROG_VERSION_STRING "0.2"
 #define PROG_VERSION_MAJOR (0)
-#define PROG_VERSION_MINOR (1)
+#define PROG_VERSION_MINOR (2)
 #define PROG_VERSION (PROG_NAME "_" PROG_VERSION_STRING)
 #define PROG_EMBLEM (PROG_NAME "_" PROG_VERSION_STRING "\n" PROG_URL)
 #define PROG_COPYRIGHT "\xa9 2025 Jason Benaim, GPL v2"
