827116aafb5bd50046a644bab353e9aa96fa7ad3
jason <jkbenaim@gmail.com>
2026-06-11 17:01:37 +0000

    version 0.3 introduces support for old-style packages without offsets in the idb file
    FossilOrigin-Name: b71df11b0c66d0ce28775003c330ead3f507d5de497ba03bd8e359eb14ed0ff7

--- version.h
+++ version.h
@@ -2,9 +2,9 @@
 #define PROG_NAME "uninst"
 #define PROG_URL "https://jrra.zone/uninst"
 #define PROG_EMAIL "jkbenaim@gmail.com"
-#define PROG_VERSION_STRING "0.2"
+#define PROG_VERSION_STRING "0.3"
 #define PROG_VERSION_MAJOR (0)
-#define PROG_VERSION_MINOR (2)
+#define PROG_VERSION_MINOR (3)
 #define PROG_VERSION (PROG_NAME "_" PROG_VERSION_STRING)
 #define PROG_EMBLEM (PROG_NAME "_" PROG_VERSION_STRING "\n" PROG_URL)
-#define PROG_COPYRIGHT "\xa9 2025 Jason Benaim, GPL v2"
+#define PROG_COPYRIGHT "\xa9 2025-2026 Jason Benaim, GPL v2"
